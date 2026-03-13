86af19443a2773a9234de3321242e07a9da68593
Denis Baranov <dan.baranov@gmail.com>
2026-03-13 10:32:02 +0100

    added reel to fb

--- src/main/java/dev/postproxy/sdk/param/FacebookParams.java
+++ src/main/java/dev/postproxy/sdk/param/FacebookParams.java
@@ -7,6 +7,7 @@ import dev.postproxy.sdk.model.FacebookFormat;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record FacebookParams(
         @JsonProperty("format") FacebookFormat format,
+        @JsonProperty("title") String title,
         @JsonProperty("first_comment") String firstComment,
         @JsonProperty("page_id") String pageId
 ) {
@@ -16,15 +17,17 @@ public record FacebookParams(
 
     public static class Builder {
         private FacebookFormat format;
+        private String title;
         private String firstComment;
         private String pageId;
 
         public Builder format(FacebookFormat format) { this.format = format; return this; }
+        public Builder title(String title) { this.title = title; return this; }
         public Builder firstComment(String firstComment) { this.firstComment = firstComment; return this; }
         public Builder pageId(String pageId) { this.pageId = pageId; return this; }
 
         public FacebookParams build() {
-            return new FacebookParams(format, firstComment, pageId);
+            return new FacebookParams(format, title, firstComment, pageId);
         }
     }
 }
